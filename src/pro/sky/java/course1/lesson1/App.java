ffaa2e71b3b2ac4469346ba923acc416f322ae5a
kodarov_salavat <kodarov@gmail.com>
2023-02-01 00:54:55 +0400

    lesson1.12

--- src/pro/sky/java/course1/lesson1/App.java
+++ src/pro/sky/java/course1/lesson1/App.java
@@ -1,4 +1,7 @@
 package pro.sky.java.course1.lesson1;
 
 public class App {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/pro/sky/java/course1/lesson1/Book.java
@@ -0,0 +1,7 @@
+package pro.sky.java.course1.lesson1;
+
+public class Book {
+    String title;
+    Author author;
+    int publicationYear;
+}
