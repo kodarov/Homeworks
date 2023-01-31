7928cafed121b47feedd8088f733d8f829c0bfa3
kodarov_salavat <kodarov@gmail.com>
2023-02-01 02:19:54 +0400

    lesson1.12

--- src/pro/sky/java/course1/lesson12/App.java
+++ src/pro/sky/java/course1/lesson12/App.java
@@ -2,9 +2,11 @@ package pro.sky.java.course1.lesson12;
 
 public class App {
     public static void main(String[] args) {
-Author kodarov = new Author("Салават","Кодаров");
-Book myBook = new Book("Познание в муках",kodarov,2022);
-myBook.setPublicationYear(2023);
-        //System.out.println(myBook.getTitle()+" "+myBook.getAuthor().getFirstName() + " " + myBook.getPublicationYear());
+Author kodarovS = new Author("Салават","Кодаров");
+Book myBook1 = new Book("Муки познания Java",kodarovS,2022);
+myBook1.setPublicationYear(2023);
+
+Author volkovA = new Author("Александр","Волков");
+Book myBook2 = new Book("Как заработать миллион$ программистом",volkovA, 1996);
     }
 }
