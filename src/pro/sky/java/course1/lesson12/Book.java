24c753bf77f2ca8fd53a722a37404cd34731e410
kodarov_salavat <kodarov@gmail.com>
2023-02-01 01:45:43 +0400

    lesson1.12

--- src/pro/sky/java/course1/lesson12/Book.java
+++ src/pro/sky/java/course1/lesson12/Book.java
@@ -2,15 +2,27 @@ package pro.sky.java.course1.lesson12;
 
 public class Book {
    private final String title;
-   private Author author;
-   private final int publicationYear;
-   private final String authorFirstName;
-   private final String authorLastname;
+   private final Author author;
+   private int publicationYear;
    public Book(String title, Author author,int publicationYear){
       this.title = title;
       this.publicationYear=publicationYear;
-      this.authorFirstName=author.getFirstName();
-      this.authorLastname=author.getLastName();
+      this.author = author;
    }
 
+   public String getTitle() {
+      return title;
+   }
+
+   public Author getAuthor() {
+      return author;
+   }
+
+   public int getPublicationYear() {
+      return publicationYear;
+   }
+
+   public void setPublicationYear(int publicationYear) {
+      this.publicationYear = publicationYear;
+   }
 }
