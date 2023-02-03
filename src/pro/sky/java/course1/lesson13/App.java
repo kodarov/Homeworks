0df6c4972bd4c723e900f09d4cdcb7b1917a3b67
kodarov_salavat <kodarov@gmail.com>
2023-02-03 17:04:39 +0400

    lesson1.13

--- src/pro/sky/java/course1/lesson13/App.java
+++ src/pro/sky/java/course1/lesson13/App.java
@@ -2,14 +2,14 @@ package pro.sky.java.course1.lesson13;
 
 public class App {
     public static void main(String[] args) {
-Author kodarovS = new Author("Салават","Кодаров");
-Book myBook1 = new Book("Муки познания Java",kodarovS,2022);
-myBook1.setPublicationYear(2023);
+        Author kodarovS = new Author("Салават", "Кодаров");
+        Book myBook1 = new Book("Муки познания Java", kodarovS, 2022);
+        myBook1.setPublicationYear(2023);
 
-Author volkovA = new Author("Александр","Волков");
-Book myBook2 = new Book("Как заработать миллион$ программистУ",volkovA, 1996);
+        Author volkovA = new Author("Александр", "Волков");
+        Book myBook2 = new Book("Как заработать миллион$ программистУ", volkovA, 1996);
 
-        System.out.println("\""+myBook1.getTitle()+"\", "+myBook1.getAuthor().getFirstName() +" "+ myBook1.getAuthor().getLastName()+", " + myBook1.getPublicationYear());
+        System.out.println("\"" + myBook1.getTitle() + "\", " + myBook1.getAuthor().getFirstName() + " " + myBook1.getAuthor().getLastName() + ", " + myBook1.getPublicationYear());
         /* print lesson13 */
         System.out.println(myBook1);
         System.out.println(myBook2);
