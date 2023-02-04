60d70f5b3717a466e6bbf49724487aafccb45128
kodarov_salavat <kodarov@gmail.com>
2023-02-04 18:26:06 +0400

    Employee

--- src/pro/sky/java/course1/termpaper/Employee.java
+++ src/pro/sky/java/course1/termpaper/Employee.java
@@ -4,12 +4,51 @@ public class Employee {
     private final String firstName;
     private final String lastName;
     private final String surname;
+    private static int count = 0;
     private double salary;
-    private int departament;
+    private int dep;
 
-    public Employee(String lastName, String firstName, String surname, int departament, double salary) {
+    public Employee(String firstName, String lastName, String surname, int dep, double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.surname = surname;
+        this.dep = dep;
+        this.salary = salary;
+        count++;
+    }
+    public Employee(String lastName, String firstName, String surname) {
+this(lastName,firstName,surname,0, 0L);
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getDep() {
+        return dep;
+    }
+
+    public void setDep(int dep) {
+        this.dep = dep;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
 }
--- src/pro/sky/java/course1/termpaper/Main.java
+++ src/pro/sky/java/course1/termpaper/Main.java
@@ -1,6 +1,6 @@
 package pro.sky.java.course1.termpaper;
 
-public class App {
+public class Main {
     public static void main(String[] args) {
 
     }
