3723d522f1ddfbe1b76065cbc6fcdf0f4909fbd8
kodarov_salavat <kodarov@gmail.com>
2023-02-07 01:11:22 +0400

    term-paper

--- src/pro/sky/java/course1/termpaper/CalcEmployee.java
+++ src/pro/sky/java/course1/termpaper/CalcEmployee.java
@@ -2,16 +2,41 @@ package pro.sky.java.course1.termpaper;
 
 public class CalcEmployee {
     static double sum;
+    static double min;
+    static double max;
+    static double average;
     public static void printEmployeeBook(Employee[] employees) {
         for (Employee employee : employees) {
             System.out.println(employee.toString());
         }
     }
-    public static double calcSalariesAll(Employee[] employees){
-        for (Employee employee : employees) {
-            sum = sum + employee.getSalary();
+    public static double calculationSumSalaries(Employee[] employees){
+        for (Employee i : employees) {
+            sum = sum + i.getSalary();
         }
         return sum;
+    }
+    public static double calculationMinSalaries(Employee[] employees){
+        min = employees[0].getSalary();
+        for (int i = 0; i < employees.length-1; i++) {
+            if (employees[i].getSalary()<min){
+                min = employees[i].getSalary();
+            }
+        }
+        return min;
+    }
+    public static double calculationMaxSalaries(Employee[] employees){
+        max = employees[0].getSalary();
+        for (int i = 0; i < employees.length-1; i++) {
+            if (employees[i].getSalary()>max){
+                max = employees[i].getSalary();
+            }
+        }
+        return max;
+    }
+    public static double calculationAverageSalaries(Employee[] employees){
+        average = calculationSumSalaries(employees) /employees.length;
+        return average;
     }
 
 }
